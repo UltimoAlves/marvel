38a6cf674f1fc515953da38f781eee0378271423
UltimoAlves <ufsalves@gmail.com>
2023-04-30 00:52:23 -0400

    Initial Commit

---
+++ MarvelTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelTest
+//
+//  Created by Ultimo Alves on 30/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
